f4789505b41a35ed3bb859d6074a41a36b8cb54b
pablo <pabmoranc@alu.edu.gva.es>
2025-01-22 23:02:48 +0100

    Acabado ejercicio 4

---
+++ src/Cuenta.java
@@ -0,0 +1,40 @@
+public class Cuenta {
+
+    private String titular;
+    private double cantidad;
+
+
+     public Cuenta(String titular, double cantidad){
+         this.titular = titular;
+         this.cantidad = cantidad;
+     }
+
+     public Cuenta(String titular){
+         this.titular = titular;
+     }
+
+    public void ingresar(double cantidad){
+         if(cantidad >0){
+             this.cantidad =this.cantidad + cantidad;
+         }
+    }
+
+    public void retirar(double cantidad){
+         if(cantidad>0) {
+
+
+             if (this.cantidad - cantidad < 0) {
+                 this.cantidad = 0;
+             } else {
+                 this.cantidad = this.cantidad - cantidad;
+             }
+         }
+    }
+
+    @Override
+    public String toString(){
+
+         return titular+"\n"+ cantidad+"$";
+    }
+
+}
---
+++ src/mainCuenta.java
@@ -0,0 +1,18 @@
+public class mainCuenta {
+    public static void main(String[] args) {
+
+        Cuenta cuenta1 = new Cuenta("Pepe");
+
+        Cuenta cuenta2 = new Cuenta("Manolo",453);
+
+
+
+        cuenta1.ingresar(100);
+        System.out.println(cuenta1);
+
+
+        cuenta2.retirar(100);
+
+        System.out.println(cuenta2);
+    }
+}
